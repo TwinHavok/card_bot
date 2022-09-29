442df2b3f02449de2f2d7572d8e7a2d54c00f872
Enlightened-Havok <erik_manley@hotmail.com>
2022-09-28 23:04:20 -0600

    set a custom bot activity

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 // When the client is ready, run this code (only once)
 client.once("ready", () => {
   console.log("The bot is running");
+  client.user.setActivity("Type /draw to draw a card");
 });
 
 client.on("interactionCreate", async (interaction) => {
